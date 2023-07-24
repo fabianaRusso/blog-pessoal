281c888ae224c40a1e4e04da898be5603ae15cba
fabianaRusso <fabi.pereiraa@hotmail.com>
2023-07-24 16:14:43 -0300

    Criando o primeiro componente

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src='https://i.pinimg.com/originals/a8/21/c6/a821c6d8f292564abf3bda488a79518c.jpg' alt='Nebulosa -Olho de Deus' className='img'/>
+           
+        </>
+    );
+}
+
+export default Home;
